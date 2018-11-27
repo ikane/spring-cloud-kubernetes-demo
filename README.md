a26541ecbd09b282c57b7eb45a0bc15567def1e9
ikane <ibrahima.kane@verifone.com>
2018-11-27 15:41:27 +0100

    updates

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # spring-cloud-kubernetes-demo
+
+https://www.baeldung.com/spring-boot-minikube
+
+
+https://github.com/eugenp/tutorials/tree/master/spring-cloud/spring-cloud-kubernetes
---
+++ demo-frontend/src/main/java/com/ikane/demofrontend/DemoFrontendApplication.java
@@ -0,0 +1,12 @@
+package com.ikane.demofrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFrontendApplication.class, args);
+	}
+}
